7d066be0f2021e42ae351432c99a7c77639a7b4b
Divyanshu-Mathur <divyanshumathur2004@gmail.com>
2025-02-23 10:33:35 +0530

    22

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -9,8 +9,8 @@ class PredictPipeline:
     
     def predict(self,features):
         try:
-            model_path = 'artifacts\\model.pkl'
-            preprocessor_path = 'artifacts\\preprocessor.pkl'
+            model_path = os.path.join("artifacts", "model.pkl")
+            preprocessor_path = os.path.join("artifacts", "preprocessor.pkl")
             model = load_obj(file_path=model_path)
             preprocessor = load_obj(file_path=preprocessor_path)
             data = preprocessor.transform(features)
